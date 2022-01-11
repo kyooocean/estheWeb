1f0b9d46096d1c39483d22a646b4973b5913ddc2
Kyohei Morinaka <86336347+kyooocean@users.noreply.github.com>
2022-01-11 10:01:30 +0900

    Add files via upload

---
+++ ikoi.js
@@ -0,0 +1,12 @@
+function initparticles() {
+   bubbles();
+
+function bubbles() {
+   $.each($(".particletext.bubbles"), function(){
+      var bubblecount = ($(this).width()/50)*10;
+      for(var i = 0; i <= bubblecount; i++) {
+         var size = ($.rnd(40,80)/10);
+         $(this).append('<span class="particle" style="top:' + $.rnd(20,80) + '%; left:' + $.rnd(0,95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
+      }
+   });
+}
